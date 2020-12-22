d4b4b97ac188de8c3007f45e11ca6099b5476f9b
Norber <nox_nox_@hotmail.com>
2020-12-22 13:13:58 +0000

    Added axios to connect with API. Done background and query builder scripts

--- js/background.js
+++ js/background.js
@@ -1,33 +1,197 @@
-import { QueryBuilder } from "./model/QueryBuilder";
+import * as QueryBuilder from "./model/QueryBuilder";
 
-chrome.tabs.onCreated.addListener(tab => {
-
+const axios = require('axios').default;
+const api = axios.create({
+  baseURL: "http://localhost:3000/",
+  timeout: 4000
 })
+const HTML_NAME = 'tabsManagement.html', CHROMEID_NAME = 'currentChromeId', TABID_NAME = 'tabId'
+var mtCreated = null, dragging = false, draggingTimer = null, startOk = false
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//#region helpers
+/**
+ * Check if tab is management tab
+ * @param {chromeTab} tab Tab to check
+ */
+const tabIsMT = tab => {
+  console.log("ISMT ", tab)
+  return tab.url.slice(tab.url.length - HTML_NAME.length) === HTML_NAME;
+}
+/**
+ * Open new tab
+ * @param {string} url Tab url
+ */
+const openInNewTabOrSelectExistingTab = url => {
+  console.log()
+  console.log("%%%%%%%% NEW TAB")
+  //chrome.tabs.query({}, function (tabs) { console.log(tabs) });
+  if (!mtCreated) {
+    chrome.tabs.create({ "url": url });
+    mtCreated = true;
+  } else {
+    //*********** TODO **********************/
+    //SELECT MANAGEMENT TAB
+  }
+};
+//#endregion
 
-})
+if(startOk) {
+  /**
+   * Event for opening extension management tab
+   */
+  chrome.browserAction.onClicked.addListener(() => {
+    console.log("ICON CLICK")
+    openInNewTabOrSelectExistingTab("./" + HTML_NAME);
+  });
 
-chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+  chrome.tabs.onCreated.addListener(tab => {
+    console.log('tab created ', tab)
+    const newTab = QueryBuilder.getObjectFromTab(tab)
 
-})
+    if (!mtCreated && tabIsMT(tab))
+      mtCreated = newTab
 
-chrome.tabs.onAttached.addListener((tabId, attachInfo) => {
+    api.post(`tab?${CHROMEID_NAME}=${tab.windowId}`, newTab)
+      .then(res => {
+        console.log(`Tab created succesfully in BD with response:\n${res}`)
+      })
+      .catch(err => {
+        console.error(`Error creating tab in BD:\n${err}`)
+      })
+  })
 
-})
+  chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    console.log('tab removed ', tabId)
+    if (mtCreated && mtCreated.tabId === tabId)
+      mtCreated = null
 
-chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
+    api.delete(`tab?${CHROMEID_NAME}=${removeInfo.windowId}&${TABID_NAME}=${tabId}`)
+      .then(res => {
+        console.log(`Tab removed succesfully in BD with response:\n${res}`)
+      })
+      .catch(err => {
+        console.error(`Error removing tab in BD:\n${err}`)
+      })
+  })
+  /*
+  const tabSchema = new Schema({
+    url: {type: String, required: true},
+    tabId: {type: String, required: true},
+    title: {type: String, required: true},
+    muted: {type: Boolean, required: true},
+    pinned: {type: Boolean, required: true},
+    selected: {type: Boolean, required: true}
+  })
+  const windowSchema = new Schema({
+    tabs: {type: [tabSchema], required: true},
+    currentChromeId: {type: String, required: true},
+    creationDate: {type: Date, required: true}
+  })
+  */
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    let updatesObject = {}
+    if (changeInfo.mutedInfo)
+      updatesObject[muted] = changeInfo.mutedInfo.muted
+    if (changeInfo.pinned)
+      updatesObject[pinned] = changeInfo.pinned
+    if (changeInfo.title)
+      updatesObject[title] = changeInfo.title
+    if (changeInfo.url)
+      updatesObject[url] = changeInfo.url
 
-})
+    api.patch(`tab?${CHROMEID_NAME}=${tab.windowId}`, QueryBuilder.bodyPatchTabFromWindow(tabId, updatesObject))
+      .then(res => {
+        console.log(`Tab updated succesfully in BD with response:\n${res}`)
+      })
+      .catch(err => {
+        console.error(`Error updating tab in BD:\n${err}`)
+      })
+  })
 
-chrome.windows.onRemoved.addListener(windowId => {
+  chrome.tabs.onAttached.addListener((tabId, attachInfo) => {
+    chrome.tabs.get(tabId, tab => {
+      api.post(`tab?${CHROMEID_NAME}=${attachInfo.newWindowId}`, QueryBuilder.getObjectFromTab(tab))
+        .then(res => {
+          console.log(`Attahed tab created succesfully in BD with response:\n${res}`)
+        })
+        .catch(err => {
+          console.error(`Error creating attached tab in BD:\n${err}`)
+        })
+    })
+  })
 
-})
+  chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
+    api.delete(`tab?${CHROMEID_NAME}=${detachInfo.oldWindowId}&${TABID_NAME}=${tabId}`)
+      .then(res => {
+        console.log(`Detached tab removed succesfully in BD with response:\n${res}`)
+      })
+      .catch(err => {
+        console.error(`Error removing detached tab in BD:\n${err}`)
+      })
 
-chrome.windows.onCreated.addListener(window => {
-  
-})
+    dragging = true;
+    draggingTimer = setTimeout(() => { dragging = false }, 5000);
+  })
 
-chrome.tabs.query({}, tabs => {
+  chrome.windows.onCreated.addListener(window => {
+    api.post('window', QueryBuilder.getObjectFromWindow(window))
+      .then(res => {
+        console.log(`Window created succesfully in BD with response:\n${res}`)
+      })
+      .catch(err => {
+        console.error(`Error creating window in BD:\n${err}`)
+      })
+  })
 
+  chrome.windows.onRemoved.addListener(windowId => {
+    if(dragging) {
+      api.delete(`window?${CHROMEID_NAME}=${windowId}`)
+        .then(res => {
+          console.log(`Window removed succesfully in BD with response:\n${res}`)
+        })
+        .catch(err => {
+          console.error(`Error removing window in BD:\n${err}`)
+        })
+      
+      clearTimeout(draggingTimer)
+      dragging = false
+    }
+  })
+}
+
+chrome.tabs.query({}, async (tabs) => {
+  let bdWindow;
+  try {
+    bdWindow = await api.get('windowByURLs', QueryBuilder.bodyGetWindowByUrls(tabs))
+  } catch(err) {
+    if(err === 'Not found window') {
+      chrome.windows.getCurrent({}, current => {
+        api.post('window', QueryBuilder.getObjectFromWindow(current))
+          .then(res => {
+            console.log(`Window created succesfully in BD with response:\n${res}`)
+          })
+          .catch(err => {
+            startOk = false
+            console.error(`Error creating window in BD:\n${err}`)
+          })
+      })
+    } else {
+      startOk = false
+      console.error(`Error getting window by urls:\n${err}`)
+    }
+
+    return;
+  }
+
+  Promise.all([
+    api.patch(`windowChromeId?_id=${bdWindow._id}`, QueryBuilder.bodyPatchWindowChromeId(tabs[0].windowId)),
+    api.patch(`tabsIds?_id=${bdWindow._id}`, QueryBuilder.bodyPatchUpdateTabsIds(bdWindow, tabs))
+  ])
+    .then((res1, res2) => {
+      console.log(`Done starting window's ids update:\n${res1}\n\n${res2}`)
+    })
+    .catch(err => {
+      startOk = false
+      console.error(`Error starting window's ids update:\n${err}`)
+    })
 })
--- js/model/QueryBuilder.js
+++ js/model/QueryBuilder.js
@@ -1,8 +1,9 @@
-export const bodyGetWindowByUrls = urlsArray => {
+export const bodyGetWindowByUrls = tabs => {
+  let urlsArray = tabs.map(x => x.url)
   return {
     $and: [
       {
-        tabs: { $size: urlsArray.length }
+        'tabs': { $size: urlsArray.length }
       },
       {
         'tabs.url': { $all: urlsArray }
@@ -15,4 +16,37 @@ export const bodyPatchTabFromWindow = (tabId, updatesObject) => {
 }
 export const bodyPatchWindowChromeId = newChromeId => {
   return { currentChromeId: newChromeId }
+}
+export const bodyPatchUpdateTabsIds = (bdWindow, tabs) => {
+  /*
+  body = {
+    oldTabId: newTabId,
+    ...
+  }
+  */
+  let body = {}
+  bdWindow.tabs.forEach(tab => {
+    let tabIndex;
+    if((tabIndex = tabs.findIndex(x => x.url === tab.url)) !== -1) {
+      body[tab.tabId] = tabs[tabIndex].tabId
+    }
+  })
+  return body
+}
+export const getObjectFromTab = tab => {
+  return {
+    url: tab.url,
+    tabId: tab.tabId,
+    title: tab.title,
+    muted: tab.mutedInfo.muted,
+    pinned: tab.pinned,
+    selected: tab.selected
+  }
+}
+export const getObjectFromWindow = myWindow => {
+  return {
+    tabs: myWindow.tabs.map(x => getObjectFromTab(x)),
+    currentChromeId: myWindow.id,
+    creationDate: Date.now()
+  }
 }
