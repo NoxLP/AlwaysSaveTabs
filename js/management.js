cebdd0f8bf904da20b237e549a88d86275842e68
Norber <nox_nox_@hotmail.com>
2020-12-27 10:27:50 +0000

    Done management HTML and script to fill it.

--- js/management.js
+++ js/management.js
@@ -10,38 +10,59 @@ const CHECKED_CHECKBOXES = {
   last: 0,
   selectedIds: function () { return CHECKED_CHECKBOXES.CBs.filter(x => document.getElementById(x).checked) }
 };
-const WINDOWS_SECTION_ID = 'windowsSection'
+const WINDOWS_ID_SECTION = 'windowsSection', WINDOWS_ID_EDIT_TITLE = 'editWindowTitleB', TABS_ID_CHECKBOX = 'tabCBox', CHROMEID_NAME = 'currentChromeId'
 var shift = false, checkedByScript = false, filterCaseSensitive = false;
 
 //#region helpers
 /*
-<div class="accordion-item">
-  <h2 class="accordion-header" id="header145">
-    <button id="window145" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse145" aria-expanded="true" aria-controls="collapse145">
-      Nombre ventana - fecha ventana
-    </button>
-  </h2>
-  <div class="d-flex flex-column">
-    <div class="d-flex flex-row justify-content-between align-items-center mx-2">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="tab652CBox">
-      </div>
-      <img class="img-thumbnail ms-3" src="#" alt="web icon">
-      <a class="flex-grow-1" target="_blank" href="${tab.url}">
-        <span class="ms-3">tab.title</span>
-        <span class="ms-3">domain</span>
-      </a>
-      <br>
-    </div>
-  </div>
-</div>
+<section id="windowsSection" class="accordion mt-4 flex-grow-1 mx-2 mx-md-3 mx-xl-5">
+  <div class="accordion-item">
+    
+    
+          
+        </div>
+</section>
 */
+const buildWindowEditButtonId = myWindow => `${WINDOWS_ID_EDIT_TITLE}${myWindow[CHROMEID_NAME]}`
+const buildTabCheckboxId = tab => `${TABS_ID_CHECKBOX}${tab.tabId}`
 const createWindowHTML = myWindow => {
-  let windowsSection = document.getElementById(WINDOWS_SECTION_ID)
-  
+  let windowsSection = document.getElementById(WINDOWS_ID_SECTION)
+  let editTitleButtonId = buildWindowEditButtonId(myWindow)
+  let headerId = `header${myWindow[CHROMEID_NAME]}`
+  let windowDiv = document.createElement('div')
+  windowDiv.classList.add('accordion-item')
+  windowDiv.innerHTML = `<h2 class="accordion-header d-flex flex-row border" id="${headerId}">
+  <button class="btn mb-1" id="${editTitleButtonId}">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#0c63e4" class="bi bi-pencil-square" viewBox="0 0 16 16">
+      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+    </svg>
+  </button>
+  <button id="windowB${myWindow[CHROMEID_NAME]}" class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${myWindow[CHROMEID_NAME]}" aria-expanded="true" aria-controls="collapse${myWindow[CHROMEID_NAME]}">
+  ${myWindow.title ? myWindow.title : ''} - ${(new Date(myWindow.creationDate)).toLocaleDateString()}
+  </button>
+</h2>
+<div class="accordion-collapse collapse show" aria-labelledby="${headerId}" data-bs-parent="#windowsSection">
+  <div class="accordion-body d-flex flex-column">
+      ${myWindow.tabs.map(tab => buildTabElements(tab)).join('\n')}
+  </div>
+</div>`
+  windowsSection.appendChild(windowDiv)
 }
-const createTabElementIn = (element, tab) => {
+const buildTabElements = tab => {
+  let domain = tab.url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im)[0];
+  let icon = "http://s2.googleusercontent.com/s2/favicons?domain_url=" + domain;
 
+  return `<div class="d-flex flex-row justify-content-between align-items-center mx-2 mt-2">
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="${buildTabCheckboxId(tab)}">
+  </div>
+  <img class="img-thumbnail ms-3" src="${icon}" alt="web icon">
+  <a class="flex-grow-1" target="_blank" href="${tab.url}">
+    <span class="ms-3">${tab.title}   ;   ${tab.url}</span>
+    <span class="ms-3">${domain}</span>
+  </a>
+</div>`
 }
 //#endregion
 
@@ -93,12 +114,12 @@ const initialBDError = () => {
 (async function buildHTML() {
   let myWindows;
   try {
-    myWindows = api.get('windows')
+    myWindows = (await api.get('windows')).data
   } catch(err) {
     initialBDError()
     return
   }
-  
+  console.log(myWindows)
   myWindows.forEach(x => {
     createWindowHTML(x)
   })
